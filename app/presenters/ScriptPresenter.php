ccfa42e123ae8c5693e2cb02eae135db07114a75
David Bittner <david.bittner@4every1.cz>
2016-12-22 12:46:04 +0100

    Update expiration

--- app/presenters/ScriptPresenter.php
+++ app/presenters/ScriptPresenter.php
@@ -15,7 +15,7 @@ class ScriptPresenter extends \Nette\Application\UI\Presenter
     public function actionMaster()
     {
         $this->response->setHeader('Content-Type', 'application/javascript');
-        $this->response->setExpiration(3600);
+        $this->response->setExpiration('+ 1 hours');
     }
     
 }
